9494cc11568472d441bef11ddb60457b6715e302
Piyush Shah <piyush.shah@espressif.com>
2020-10-08 00:48:28 +0530

    esp_hap_platform: Minor restructuring
    esp_hap_platform had an src/esp32 folder. However, the same source files work
    for esp32, esp32s2 as well as esp8266. So, no need of having this folder

--- components/homekit/esp_hap_platform/component.mk
+++ components/homekit/esp_hap_platform/component.mk
@@ -2,8 +2,8 @@
 # Component Makefile
 #
 CFLAGS += -Wno-unused-function
-COMPONENT_SRCDIRS := src/esp32
+COMPONENT_SRCDIRS := src
 ifdef CONFIG_IDF_TARGET_ESP8266
-COMPONENT_OBJEXCLUDE += src/esp32/esp_mfi_i2c.o
+COMPONENT_OBJEXCLUDE += src/esp_mfi_i2c.o
 endif
 COMPONENT_ADD_INCLUDEDIRS := include
